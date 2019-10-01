52c8fdc607bca513d49a8905349500324a6935e2
devinhaeng <hoonbitcoin@gmail.com>
2019-10-01 12:37:34 +0900

    Getting the Weather

--- App.js
+++ App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { Alert } from "react-native";
 import Loading from './Loading';
 import * as Location from 'expo-location';
-import { isLoading } from 'expo-font';
+import axios from 'axios';
+
+const API_KEY = "dcc9249aff94f29b4ebb11d4169d5ee7";
 
 export default class extends React.Component {
   state = {
-    isLoding: true
+    isLoading: true
+  };
+  getWeather = async (latitude, longtitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
   };
   getLocation = async () => {
     try {
@@ -14,7 +22,8 @@ export default class extends React.Component {
       const {
         coords: { latitude, longtitude }
       } = await Location.getCurrentPositionAsync();
-      this.setState({ isLoding: flase });
+      this.getWeather(latitude, longtitude);
+      this.setState({ isLoading: flase });
     } catch (error) {
       Alert.alert("Can't find you", "So sad");
     }
@@ -23,7 +32,7 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoding } = this.state;
+    const { isLoading } = this.state;
     return isLoading ? <Loading /> : null;
   }
 }
