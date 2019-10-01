a62a7f1186bb729ffaff31409861f5909c538bcf
devinhaeng <hoonbitcoin@gmail.com>
2019-10-01 12:09:03 +0900

    Asking for Permissions

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { Alert } from "react-native";
 import Loading from './Loading';
 import * as Location from 'expo-location';
+import { isLoading } from 'expo-font';
 
 export default class extends React.Component {
+  state = {
+    isLoding: true
+  };
   getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longtitude }
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoding: flase });
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad");
+    }
   };
   componentDidMount() {
     this.getLocation();
   }
   render() {
-    return <Loading />;
+    const { isLoding } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
